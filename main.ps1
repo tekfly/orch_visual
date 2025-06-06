72f7b50056823a85682801451520dd90c506331c
tekfly <andrefsousa1992@gmail.com>
2025-06-06 13:05:37 +0100

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -27,8 +27,8 @@ $xamlFiles = @(
     @{ Url = "https://raw.githubusercontent.com/tekfly/orch_visual/refs/heads/main/xaml_files/DownloadWindow.xaml";    FileName = "DownloadWindow.xaml" },
     @{ Url = "https://raw.githubusercontent.com/tekfly/orch_visual/refs/heads/main/xaml_files/ComponentOptions.xaml";  FileName = "ComponentOptions.xaml" },
     @{ Url = "https://raw.githubusercontent.com/tekfly/orch_visual/refs/heads/main/xaml_files/InstallTypeDialog.xaml"; FileName = "InstallTypeDialog.xaml" },
-    @{ URL = "https://raw.githubusercontent.com/tekfly/orch_visual/refs/heads/main/xaml_files/Connect.xaml"; FileName ="ConnectWindow.xaml"}
-    @{ URL = "https://raw.githubusercontent.com/tekfly/orch_visual/refs/heads/main/xaml_files/WaitingWindow.xaml"; FileName ="WaitingWindow.xaml"}
+    @{ Url = "https://raw.githubusercontent.com/tekfly/orch_visual/refs/heads/main/xaml_files/Connect.xaml"; FileName ="ConnectWindow.xaml"}
+    @{ Url = "https://raw.githubusercontent.com/tekfly/orch_visual/refs/heads/main/xaml_files/WaitingWindow.xaml"; FileName ="WaitingWindow.xaml"}
 )
 
 function Download-Files {
